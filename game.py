7e85b07b264e6fac0bf0781e0e4cd5e92764d6fa
po100lit <po100lit@mail.ru>
2022-08-14 01:08:26 +0600

    upload video

--- game.py
+++ game.py
@@ -2,71 +2,33 @@ import random
 
 
 # Проверяем на повторяющиеся цифры: True, если повторов нет
-def noDuplicates(num):
-    num_li = getDigits(num)
-    if len(num_li) == len(set(num_li)):
-        return True
-    else:
-        return False
+def check_duplicates(num: int) -> bool:
+    num_li = get_digits(num)
+    return len(num_li) == len(set(num_li))
 
 
 # Создаём список из цифр числа
-def getDigits(num):
+def get_digits(num: int) -> list:
     return [int(i) for i in str(num)]
 
 
 # Генерируем четырехзначное число
-def generateNum():
+def generate_num() -> int:
     while True:
         num = random.randint(1000, 9999)
-        if noDuplicates(num):
-            # print(num)  # показывает загаданное компьютером число
+        if check_duplicates(num):
             return num
 
 
-num = generateNum()
-tries = int(input("Введите количество попыток: "))
-
-
 # Поиск быков и коров
-def numOfBullsCows(num, guess):
+def bulls_and_cows(num: int, guess: int) -> list[int]:
     bull_cow = [0, 0]
-    num_li = getDigits(num)
-    guess_li = getDigits(guess)
-    for i, j in zip(num_li, guess_li):
-        # есть ли цифра в списке
-        if j in num_li:
-            # цифра есть и стоит на своём месте
-            if j == i:
+    num_list = get_digits(num)
+    guess_list = get_digits(guess)
+    for i, j in zip(num_list, guess_list):
+        if j in num_list:  # есть ли цифра в списке
+            if j == i:  # цифра есть и стоит на своём месте
                 bull_cow[0] += 1
-            # цифра есть, но стоит не на своём месте
-            else:
+            else:  # цифра есть, но стоит не на своём месте
                 bull_cow[1] += 1
     return bull_cow
-
-
-# Играем пока не кончатся попытки или не угадаем число
-while tries > 0:
-    guess = int(input("Введите число: "))
-    if not noDuplicates(guess):
-        print("Цифры не должны повторяться")
-        continue
-    if guess < 1000 or guess > 9999:
-        print("Загаданное число состоит из 4 цифр")
-        continue
-    bull_cow = numOfBullsCows(num, guess)
-    print(f"{bull_cow[0]} бык(а), {bull_cow[1]} корова(ы)")
-    tries -= 1
-    if bull_cow[0] == 4:
-        print("Поздравляю! Вы угадали!")
-        break
-else:
-    print(f"У вас кончились попытки. Было загадано число {num}")
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
